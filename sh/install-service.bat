3615a17c546efe1b70f328f31d0f9d674a8fb893
Jop <jopwork@gmail.com>
2024-08-16 16:26:19 +0800

    Add service scripts

---
+++ sh/install-service.bat
@@ -0,0 +1,15 @@
+%~dp0prunsrv.exe //IS//HelloService ^
+    --Description "HelloService" ^
+    --Startup manual ^
+    --Classpath "%~dp0webapp-0.0.1-SNAPSHOT.jar" ^
+    --StartMode jvm ^
+    --StartClass org.springframework.boot.loader.launch.JarLauncher ^
+    --StopMode jvm ^
+    --StopClass org.springframework.boot.loader.launch.JarLauncher ^
+    --StopParams stop ^
+    --ServiceUser "LocalSystem" ^
+    --LogPath "%~dp0log" ^
+    --LogLevel Debug ^
+    --JavaHome "C:\Program Files\Amazon Corretto\jdk17.0.12_7" ^
+    --StdOutput auto ^
+    --StdError auto
--- src/main/java/com/jopwork/webapp/WebappApplication.java
+++ src/main/java/com/jopwork/webapp/WebappApplication.java
@@ -2,10 +2,42 @@ package com.jopwork.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static java.lang.System.out;
 
 @SpringBootApplication
 public class WebappApplication {
+    private static ConfigurableApplicationContext context = null;
+
     public static void main(String[] args) {
-        SpringApplication.run(WebappApplication.class, args);
+
+        String command = "start";
+        if (args.length > 0) {
+            command = args[0];
+        }
+
+        long processId = ProcessHandle.current().pid();
+
+        switch (command) {
+            case "start":
+                out.printf("PID %s: Starting the application...%n", processId);
+                context = SpringApplication.run(WebappApplication.class, args);
+                out.printf("PID %s: The Spring Application has started. Context: %s%n", processId, context.toString());
+                break;
+            case "stop":
+                out.printf("PID %s: Stopping the application..., Context: %s%n", processId, context == null ? "null" : context.toString());
+                if (context != null) {
+                    out.printf("PID %s: Stopping the SpringApplicationContext...%n", processId);
+                    SpringApplication.exit(context, () -> 0);
+                    context = null;
+                }
+                out.printf("PID %s: System.exit()...%n", processId);
+                System.exit(0);
+                break;
+            default:
+                out.printf("PID %s: Invalid Command: %s%n", processId, command);
+                break;
+        }
     }
 }
